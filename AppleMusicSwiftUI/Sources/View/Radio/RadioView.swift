b9b7e72dbf7401a42e7529a8de747136c2bc9db2
Serhii Tkachenko <doker.tk@gmail.com>
2022-12-18 21:27:00 +0100

    Add HorizontalGridView and VerticalGridView

--- AppleMusicSwiftUI/Sources/View/Radio/RadioView.swift
+++ AppleMusicSwiftUI/Sources/View/Radio/RadioView.swift
@@ -8,13 +8,73 @@
 import SwiftUI
 
 struct RadioView: View {
-    var body: some View {
-        Text("Radio")
-    }
+
+	var body: some View {
+		NavigationStack {
+			ScrollView {
+				Divider()
+				HorizontalGridView()
+					.frame(height: 300)
+				Divider()
+				VerticalGridView()
+			}
+			.navigationTitle("Radio")
+		}
+	}
 }
 
 struct RadioView_Previews: PreviewProvider {
-    static var previews: some View {
-        RadioView()
-    }
+	static var previews: some View {
+		RadioView()
+	}
+}
+
+struct VerticalGridView: View {
+	var columns: [GridItem] =
+	Array(repeating: .init(.flexible()), count: 1)
+	
+	var body: some View {
+		ScrollView(.vertical) {
+			LazyVGrid(columns: columns) {
+				ForEach((1...18), id: \.self) { number in
+					HStack {
+						Image("cover")
+							.resizable()
+							.cornerRadius(24)
+							.scaledToFit()
+							.shadow(radius: 5)
+						Text("Text")
+						Spacer()
+					}
+					.frame(height: 100)
+					.padding()
+				}
+			}
+		}
+	}
+	
+}
+
+struct HorizontalGridView: View {
+	var rows: [GridItem] =
+	Array(repeating: .init(.flexible()), count: 1)
+	
+	var body: some View {
+		ScrollView(.horizontal) {
+			LazyHGrid(rows: rows) {
+				ForEach((1...18), id: \.self) { number in
+					VStack {
+						Text("Text")
+						Image("cover")
+							.resizable()
+							.cornerRadius(24)
+							.scaledToFit()
+							.shadow(radius: 5)
+					}
+					.padding()
+				}
+			}
+		}
+	}
+	
 }
