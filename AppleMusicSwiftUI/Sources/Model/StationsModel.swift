f4fa0779463b15b8f6e411929e8d82b680bb029d
Serhii Tkachenko <doker.tk@gmail.com>
2022-12-19 00:56:40 +0100

    Add Stations data

--- AppleMusicSwiftUI/Sources/Model/StationsModel.swift
+++ AppleMusicSwiftUI/Sources/Model/StationsModel.swift
@@ -17,14 +17,13 @@ struct StationsModel {
 extension StationsModel {
 
 	static var data = [
-		StationsModel(title: "indie + chill", description: "Gentle indie pop and rock melodies that make moments better.", image: "1music"),
-		StationsModel(title: "Episode 576", description: "Music from Wizkid, Kobe JT, and Cajmere.", image: "rebecco"),
-		StationsModel(title: "Iconic Duets with Sabi", description: "Iconic Duets to celebrate Apple Music Sing.", image: "hitfm"),
-		StationsModel(title: "In My Room", description: "Close the door, find a comfy corner, and get in your feelings.", image: "rock"),
-		
-		StationsModel(title: "indie + chill", description: "Gentle indie pop and rock melodies that make moments better.", image: "1music"),
-		StationsModel(title: "Episode 576", description: "Music from Wizkid, Kobe JT, and Cajmere.", image: "rebecco"),
-		StationsModel(title: "Iconic Duets with Sabi", description: "Iconic Duets to celebrate Apple Music Sing.", image: "hitfm"),
-		StationsModel(title: "In My Room", description: "Close the door, find a comfy corner, and get in your feelings.", image: "rock")
+		StationsModel(title: "Apple Music 1", description: "The new music that matters.", image: "1music"),
+		StationsModel(title: "The Country Chart Show", description: "Gentle indie pop and rock melodies that make moments better.", image: "country"),
+		StationsModel(title: "The Dotty Show", description: "Festive classics and new favourites.", image: "dotty"),
+		StationsModel(title: "Jayde Donovan", description: "Keeping your workout honest as we ramp up to the holidays.", image: "jayde"),
+		StationsModel(title: "Tokyo Highway Radio", description: "プレイリスト『Tokyo Highway：2022年』と共に、今年を振り返る.", image: "tokyo"),
+		StationsModel(title: "The Rebecca Judd Show", description: "Rebecca introduces Manchester producer [ K S R ].", image: "rebecco"),
+		StationsModel(title: "Hit FM", description: "Radio Station.", image: "hitfm"),
+		StationsModel(title: "Rock Classics Radio", description: "Jenn is joined by Gavin Rossdale of Bush.", image: "rock")
 	]
 }
